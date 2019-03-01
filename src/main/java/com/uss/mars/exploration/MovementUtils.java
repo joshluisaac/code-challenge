4b1fba6c05d60ebe443a079c8f5f2c0b0584212c
joshua <nwankwo.joshua@gmail.com>
2019-03-01 18:31:24 +1100

    Helper class containing a bunch of resuable utilities

--- src/main/java/com/uss/mars/exploration/MovementUtils.java
+++ src/main/java/com/uss/mars/exploration/MovementUtils.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ExplorerMovement {
+public class MovementUtils {
 
     private static final List<Integer> POSSIBLE_VALUES = new ArrayList<>(Arrays.asList(0,1,2,3,4));
 
     //checks if the coordinates specified satisfies the boundaries of the grid
 
-    public boolean coordinateIsValid(final int xCoordinate, final int yCoordinate) {
-        final boolean xCoordIsValid =  POSSIBLE_VALUES.contains(xCoordinate);
-        final boolean yCoordIsValid =  POSSIBLE_VALUES.contains(yCoordinate);
+    public static boolean pathIsValid(Coordinate coordinate) {
+        final boolean xCoordIsValid =  POSSIBLE_VALUES.contains(coordinate.getCoordinateX());
+        final boolean yCoordIsValid =  POSSIBLE_VALUES.contains(coordinate.getCoordinateY());
         return xCoordIsValid && yCoordIsValid;
     }
 
