9a1dd1f817419d38701aef5ddbe7cd9522260295
joshua <nwankwo.joshua@gmail.com>
2019-03-02 09:38:24 +1100

    Created command and command queue as service collaborators

--- src/main/java/com/uss/mars/exploration/Command.java
+++ src/main/java/com/uss/mars/exploration/Command.java
@@ -1,4 +1,27 @@
 package com.uss.mars.exploration;
 
 public class Command {
+
+    String name;
+    int xAxis;
+    int yAxis;
+
+    public Command(){}
+    public Command(String name, int xAxis, int yAxis){
+        this.name = name;
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getxAxis() {
+        return xAxis;
+    }
+
+    public int getyAxis() {
+        return yAxis;
+    }
 }
