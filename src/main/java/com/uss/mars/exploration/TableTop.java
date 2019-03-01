3be66c8ff519d1df27dcb1f30d3e26e6ec1a0d85
joshua <nwankwo.joshua@gmail.com>
2019-03-01 16:06:35 +1100

    Added Tabletop

--- src/main/java/com/uss/mars/exploration/TableTop.java
+++ src/main/java/com/uss/mars/exploration/TableTop.java
@@ -1,4 +1,13 @@
 package com.uss.mars.exploration;
 
+//Represented as a multi-scripted array
 public class TableTop {
+
+private static final int[][] MATRIX_GRID = {
+        {0,1,2,3,4},
+        {0,1,2,3,4},
+        {0,1,2,3,4},{0,1,2,3,4},{0,1,2,3,4}};
+
+
+
 }
