cf4b878be4b1839e5b7780c84c293532b5ac74f8
joshua <nwankwo.joshua@gmail.com>
2019-03-02 07:39:52 +1100

    Updated explorer movement with domain specific and relevant methods

---
+++ src/main/java/com/uss/mars/exploration/CommandQueueService.java
@@ -0,0 +1,4 @@
+package com.uss.mars.exploration;
+
+public class CommandQueueService {
+}
--- src/main/java/com/uss/mars/exploration/ExplorerMovement.java
+++ src/main/java/com/uss/mars/exploration/ExplorerMovement.java
@@ -1,4 +1,55 @@
 package com.uss.mars.exploration;
 
+//Explorer movement class
+//This class is dependent on the Explorer object and CommandQueueService
 public class ExplorerMovement {
+
+    private Explorer exp;
+    private CommandQueueService commandQueueService;
+
+    public ExplorerMovement(Explorer exp,CommandQueueService commandQueueService){
+        this.exp = exp;
+        this.commandQueueService = commandQueueService;
+    }
+
+    //check coordinate validity
+    //check if coordinate isn't already occupied. slotIsAvailable
+    //if true check if the head of the CommandQueue is PLACE
+    //reset blocks. That is, reinitialize the grid/TableTop.
+    //move to coordinate position
+    void occupySlot(final Explorer exp) {
+        exp.coordinateIsValid();
+        if (slotIsAvailable(exp) && commandQueueService.placeCommandIsHeadEnqued())
+        {
+            resetGrid();
+            moveToCoordinatePosition();
+        }
+    }
+
+    //Checks for slot availability
+    //Takes an explorer and then checks if the slot it is asking for is available.
+    //A slot is defined available if it is not currently being occupied by any blocks.
+    //Please note, i used the word unit in the requirements to mean slots
+    boolean slotIsAvailable(final Explorer exp){
+        return false;
+    }
+
+    SlotStatus slotAvailabilityStatus(final Explorer exp){
+
+        return SlotStatus.BLOCKED;
+    }
+
+
+    void resetGrid(){
+
+    }
+
+    void moveToCoordinatePosition(){
+
+    }
+
+
+
+
+
 }
