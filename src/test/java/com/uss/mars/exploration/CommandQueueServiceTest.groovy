96726f45b163f7d5c7f0d17da04b6cab0ee3d152
joshua <nwankwo.joshua@gmail.com>
2019-03-02 10:12:47 +1100

    Updated tests to be more expressive

--- src/test/java/com/uss/mars/exploration/CommandQueueServiceTest.groovy
+++ src/test/java/com/uss/mars/exploration/CommandQueueServiceTest.groovy
@@ -7,13 +7,16 @@ import spock.lang.Specification
 class CommandQueueServiceTest extends Specification {
 
 def 'command at head of command queue must be PLACE'(){
-    given: "an initialized command queue"
+    given: "a set of commands"
     Command cmd1 = new Command("PLACE",0,0)
     Command cmd2 = new Command("PLACE",1,2)
     Command cmd3 = new Command("BLOCK",2,2)
     Command cmd4 = new Command("BLOCK",1,4)
 
+    and: "a list of those commands"
     def commands = new ArrayList<>(Arrays.asList(cmd1,cmd2,cmd3,cmd4))
+
+    and: "an initialized command queue"
     def commandQueue = new CommandQueue()
     commandQueue.initialize(commands)
     def commandQueueService = new CommandQueueService(commandQueue)
