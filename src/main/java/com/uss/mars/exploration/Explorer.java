de8df6990a9d71d6f9570e64f6700cb9629aa10e
joshua <nwankwo.joshua@gmail.com>
2019-03-01 18:32:02 +1100

    Explorer concrete class

--- src/main/java/com/uss/mars/exploration/Explorer.java
+++ src/main/java/com/uss/mars/exploration/Explorer.java
@@ -1,4 +1,22 @@
 package com.uss.mars.exploration;
 
-public class Explorer {
+public class Explorer extends AbstractTableTopOccupant {
+
+    OccupantType type;
+    Coordinate coordinate;
+
+    public Explorer(){
+        type = OccupantType.EXPLORER;
+    }
+
+
+    @Override
+    boolean coordinateIsValid(Coordinate coordinate) {
+        return MovementUtils.pathIsValid(coordinate);
+    }
+
+    @Override
+    OccupantType getOccupantType() {
+        return null;
+    }
 }
