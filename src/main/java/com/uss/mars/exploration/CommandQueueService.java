0764c845478bcc280ba08ea35028bfbff0dd079a
joshua <nwankwo.joshua@gmail.com>
2019-03-02 07:41:06 +1100

    Added command queue service to handle command queue related methods

--- src/main/java/com/uss/mars/exploration/CommandQueueService.java
+++ src/main/java/com/uss/mars/exploration/CommandQueueService.java
@@ -1,4 +1,14 @@
 package com.uss.mars.exploration;
 
 public class CommandQueueService {
+
+    public CommandQueueService(){
+
+    }
+
+    //checks if the front of the CommandQueue is a PLACE command.
+    boolean placeCommandIsHeadEnqued(){
+        return false;
+    }
+
 }
