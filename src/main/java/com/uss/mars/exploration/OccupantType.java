f7041fad9b65dbf1b5d432509f749dfae2162181
joshua <nwankwo.joshua@gmail.com>
2019-03-01 18:32:39 +1100

    Added Occuptant type enum which is a contant of all the various occupant types

--- src/main/java/com/uss/mars/exploration/OccupantType.java
+++ src/main/java/com/uss/mars/exploration/OccupantType.java
@@ -1,4 +1,21 @@
 package com.uss.mars.exploration;
 
 public enum OccupantType {
+    EXPLORER("explorer",1), BLOCK("block",2);
+
+    private String name;
+    private int id;
+
+    OccupantType(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId(){return id;}
+
+
 }
