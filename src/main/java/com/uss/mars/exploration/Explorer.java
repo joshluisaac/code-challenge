49339b040f66500d2767aefe1fa842f01ed6c7e6
joshua <nwankwo.joshua@gmail.com>
2019-03-01 20:03:39 +1100

    Updated explorer class with more methods such as occupyUnit, moveToPosition etc

--- src/main/java/com/uss/mars/exploration/Explorer.java
+++ src/main/java/com/uss/mars/exploration/Explorer.java
@@ -5,19 +5,52 @@ public class Explorer extends AbstractTableTopOccupant {
     private OccupantType type;
     private Coordinate coordinate;
 
-    public Explorer(Coordinate coordinate){
+    public Explorer(Coordinate coordinate) {
         type = OccupantType.EXPLORER;
         this.coordinate = coordinate;
     }
 
+    /**
+     * Checks for coordinate validity.
+     * Coordinate is said to be valid if it's X and Y position falls within the stipulated boundaries
+     * <p>
+     * The value of this is then used to prevent explorer from destruction.
+     *
+     * @param coordinate the X,Y coordinates of the explorer.
+     * @return returns true or false.
+     *
+     */
 
     @Override
-    boolean coordinateIsValid(final Coordinate coordinate) {
-        return MovementUtils.pathIsValid(coordinate);
+    public boolean coordinateIsValid() {
+        return MovementUtils.pathIsValid(this.coordinate);
+    }
+
+    public boolean coordinateIsValid(final Coordinate coordinate) {
+        return coordinateIsValid();
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
     }
 
     @Override
-    OccupantType getOccupantType() {
-        return null;
+    public OccupantType getOccupantType() {
+        return type;
     }
+
+
+    void occupyUnit() {
+
+        //check coordinate validity
+
+        // if it is an origin path reset blocks otherwise move to position
+
+        //move to position
+        //if true reset blocks
+
+
+    }
+
+
 }
