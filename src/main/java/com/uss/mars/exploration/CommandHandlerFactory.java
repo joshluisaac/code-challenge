e29f2e610eb266aef71add49ad00e40bcdb1137e
joshua <nwankwo.joshua@gmail.com>
2019-03-02 19:14:36 +1100

    Updated references to Explorer

--- src/main/java/com/uss/mars/exploration/CommandHandlerFactory.java
+++ src/main/java/com/uss/mars/exploration/CommandHandlerFactory.java
@@ -2,20 +2,21 @@ package com.uss.mars.exploration;
 
 public class CommandHandlerFactory {
 
-    public static CommandHandler getHandler(Command command){
-        if(command.getName().equals(CommandType.PLACE.toString())) {
-            return new PlaceCommandHandler(new Explorer(new Coordinate(command.getxAxis(),command.getyAxis())),null);
+    public static CommandHandler getHandler(Command command) {
+        if (command.getName().equals(CommandType.PLACE.toString())) {
+            return new PlaceCommandHandler(new MarsExplorer(new Coordinate(command.getxAxis(), command.getyAxis())),
+                    null);
         }
 
-        if(command.getName().equals(CommandType.BLOCK.toString())) {
-            return new BlockCommandHandler(new Blocker(new Coordinate(command.getxAxis(),command.getyAxis())));
+        if (command.getName().equals(CommandType.BLOCK.toString())) {
+            return new BlockCommandHandler(new Blocker(new Coordinate(command.getxAxis(), command.getyAxis())));
         }
 
-        if(command.getName().equals(CommandType.EXPLORE.toString())) {
+        if (command.getName().equals(CommandType.EXPLORE.toString())) {
             return new ExploreCommandHandler();
         }
 
-        if(command.getName().equals(CommandType.REPORT.toString())) {
+        if (command.getName().equals(CommandType.REPORT.toString())) {
             return new ReportCommandHandler();
         }
         return null; // todo: refactor this
