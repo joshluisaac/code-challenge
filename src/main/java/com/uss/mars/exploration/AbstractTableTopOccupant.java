04c3bb8b450a04df089f5d6e211eb78d9d0c4c88
joshua <nwankwo.joshua@gmail.com>
2019-03-02 19:02:11 +1100

    Refactored code and remove redundant methods

--- src/main/java/com/uss/mars/exploration/AbstractTableTopOccupant.java
+++ src/main/java/com/uss/mars/exploration/AbstractTableTopOccupant.java
@@ -2,7 +2,5 @@ package com.uss.mars.exploration;
 
 public abstract class AbstractTableTopOccupant implements TableTopOccupant {
 
-    public abstract boolean coordinateIsValid(final Coordinate coordinate);
     public abstract boolean coordinateIsValid();
-    public abstract OccupantType getOccupantType();
 }
