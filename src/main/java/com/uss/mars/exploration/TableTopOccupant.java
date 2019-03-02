8d3e74535af9810a36e524a1b4e1bc856321de22
joshua <nwankwo.joshua@gmail.com>
2019-03-02 19:02:36 +1100

    Updated TableTop class

--- src/main/java/com/uss/mars/exploration/TableTopOccupant.java
+++ src/main/java/com/uss/mars/exploration/TableTopOccupant.java
@@ -3,6 +3,7 @@ package com.uss.mars.exploration;
 public interface TableTopOccupant {
 
     boolean coordinateIsValid(final Coordinate coordinate);
+    boolean coordinateIsValid();
     OccupantType getOccupantType();
 
 }
