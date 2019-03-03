394d3b454338a950f9df50915a138b55c496a804
joshua <nwankwo.joshua@gmail.com>
2019-03-04 01:33:05 +1100

    Added sensible log messages

--- src/main/java/com/uss/mars/exploration/CommandQueueService.java
+++ src/main/java/com/uss/mars/exploration/CommandQueueService.java
@@ -3,6 +3,7 @@ package com.uss.mars.exploration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.MessageFormat;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -13,7 +14,8 @@ public class CommandQueueService {
 
     private List<Command> commands;
     private Queue<Command> queue = new LinkedList<>();
-    public CommandQueueService(List<Command> commands){
+
+    public CommandQueueService(List<Command> commands) {
         this.commands = commands;
     }
 
@@ -25,32 +27,31 @@ public class CommandQueueService {
 
 
     //checks if the front of the CommandQueue is a PLACE command.
-    public boolean placeCommandIsHeadEnqueued(){
+    public boolean placeCommandIsHeadEnqueued() {
         return queue.peek().name.equals("PLACE");
     }
 
-    public boolean validateCoordinates(){
+    public boolean validateCoordinates() {
         for (Command command : queue) {
             final String placeCmd = CommandType.PLACE.toString();
             final String blockCmd = CommandType.BLOCK.toString();
             final String exploreCmd = CommandType.EXPLORE.toString();
-            if (command.getName().equals(placeCmd) || command.getName().equals(blockCmd) || command.getName().equals(exploreCmd) ){
-                if (!MovementUtils.coordinatePathIsValid(new Coordinate(command.getxAxis(),command.getyAxis()))){
-                    LOG.error("The specified X and Y coordinates are out of grid coverage.");
-                    throw new CoordinateOutsideBoundsException("The specified X and Y coordinates are out of grid coverage. Explorer would be destroyed.");
+            if (command.getName().equals(placeCmd) || command.getName().equals(blockCmd) || command.getName().equals(exploreCmd)) {
+                if (!MovementUtils.coordinatePathIsValid(new Coordinate(command.getxAxis(), command.getyAxis()))) {
+                    LOG.error("The specified X:{} and Y:{} coordinates for {} is out of grid coverage.", command.getxAxis(), command.getyAxis(), command.name);
+                    String errorMsg = MessageFormat.format("The specified X:{0} and Y:{1} coordinates for {2} is out of grid coverage. Explorer would be destroyed.",command.getxAxis(), command.getyAxis(), command.name);
+                    throw new CoordinateOutsideBoundsException(errorMsg);
                 }
             }
         }
+        LOG.info("Coordinates validated successfully.");
         return true;
     }
 
-    void validateQueueCommands(){
-        for (Command command : queue) {
-            LOG.info(command.getName());
-        }
+    void validateQueueCommandSyntax() {
     }
 
-    public Queue<Command> getQueue(){
+    public Queue<Command> getQueue() {
         return this.queue;
     }
 }
