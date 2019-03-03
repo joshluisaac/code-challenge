28f87e279c62587fa992607bb4ef36eae285d66f
joshua <nwankwo.joshua@gmail.com>
2019-03-03 19:27:19 +1100

    Refactoring

--- src/main/java/com/uss/mars/exploration/CommandQueueService.java
+++ src/main/java/com/uss/mars/exploration/CommandQueueService.java
@@ -1,18 +1,38 @@
 package com.uss.mars.exploration;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class CommandQueueService {
 
-    private CommandQueue commandQueue;
-    public CommandQueueService(CommandQueue commandQueue){
-        this.commandQueue = commandQueue;
+    private List<Command> commands;
+    private Queue<Command> queue = new LinkedList<>();
+    public CommandQueueService(List<Command> commands){
+        this.commands = commands;
     }
+
+
+    //Takes a list of commands and then populates a queue
+    public void initialize() {
+        queue.addAll(commands);
+    }
+
+    public Command peek(){
+        return queue.peek();
+    }
+
 
     //checks if the front of the CommandQueue is a PLACE command.
     public boolean placeCommandIsHeadEnqueued(){
-        return commandQueue.peek().name.equals("PLACE");
+        return queue.peek().name.equals("PLACE");
     }
 
     void validateQueueCommands(){
 
+    }
+
+    public Queue<Command> getQueue(){
+        return this.queue;
     }
 }
