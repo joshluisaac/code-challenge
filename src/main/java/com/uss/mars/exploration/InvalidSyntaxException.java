5968da63c79aa30dd4d21d5af70df4562e5426f5
joshua <nwankwo.joshua@gmail.com>
2019-03-04 10:32:13 +1100

    Initial commit: added exception for invalid command syntax

--- src/main/java/com/uss/mars/exploration/InvalidSyntaxException.java
+++ src/main/java/com/uss/mars/exploration/InvalidSyntaxException.java
@@ -1,4 +1,34 @@
 package com.uss.mars.exploration;
 
-public class InvalidSyntaxException {
+public class InvalidSyntaxException extends RuntimeException {
+
+    /**
+     * Constructs an <code>InvalidSyntaxException</code> with the specified message.
+     *
+     * @param msg the detail message
+     */
+    public InvalidSyntaxException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs an <code>InvalidSyntaxException</code> with the specified message and
+     * root cause.
+     *
+     * @param msg the detail message
+     * @param t root cause
+     */
+    public InvalidSyntaxException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    /**
+     * Constructs an <code>InvalidSyntaxException</code> with the root cause.
+     *
+     * @param t root cause
+     */
+    public InvalidSyntaxException(Throwable cause) {
+        super(cause);
+    }
+
 }
