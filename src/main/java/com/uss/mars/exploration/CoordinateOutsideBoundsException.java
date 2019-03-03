96bf86e4efc444f35a217b5eba7fd1e8400fa194
joshua <nwankwo.joshua@gmail.com>
2019-03-04 00:20:29 +1100

    Initial commit: added custom runtime exception

--- src/main/java/com/uss/mars/exploration/CoordinateOutsideBoundsException.java
+++ src/main/java/com/uss/mars/exploration/CoordinateOutsideBoundsException.java
@@ -1,4 +1,36 @@
 package com.uss.mars.exploration;
 
-public class CoordinateOutsideBoundsException {
+public class CoordinateOutsideBoundsException extends RuntimeException {
+
+    /**
+     * Constructs an <code>CoordinateOutsideBoundsException</code> with the specified message.
+     *
+     * @param msg the detail message
+     */
+    public CoordinateOutsideBoundsException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs an <code>CoordinateOutsideBoundsException</code> with the specified message and
+     * root cause.
+     *
+     * @param msg the detail message
+     * @param t root cause
+     */
+    public CoordinateOutsideBoundsException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    /**
+     * Constructs an <code>CoordinateOutsideBoundsException</code> with the root cause.
+     *
+     * @param t root cause
+     */
+    public CoordinateOutsideBoundsException(Throwable cause) {
+        super(cause);
+    }
+
+
+
 }
