417cea6bfef346e1e6135739ae6e6f5b3eadbb3b
joshua <nwankwo.joshua@gmail.com>
2019-03-03 19:27:52 +1100

    Updated tests to reflect changes to CommandQueue

--- src/test/java/com/uss/mars/exploration/CommandQueueServiceTest.groovy
+++ src/test/java/com/uss/mars/exploration/CommandQueueServiceTest.groovy
@@ -17,9 +17,8 @@ def 'command at head of command queue must be PLACE'(){
     def commands = new ArrayList<>(Arrays.asList(cmd1,cmd2,cmd3,cmd4))
 
     and: "an initialized command queue"
-    def commandQueue = new CommandQueue()
-    commandQueue.initialize(commands)
-    def commandQueueService = new CommandQueueService(commandQueue)
+    def commandQueueService = new CommandQueueService(commands)
+    commandQueueService.initialize();
 
     when: "we query the head of the queue"
     def result = commandQueueService.placeCommandIsHeadEnqueued()
