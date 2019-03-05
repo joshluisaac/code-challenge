4468a8a49dc985219dc4d79258c5cea8d4b57853
joshua <nwankwo.joshua@gmail.com>
2019-03-05 12:46:17 +1100

    Added interface dependency

--- src/main/java/com/uss/mars/exploration/services/CommandQueueService.java
+++ src/main/java/com/uss/mars/exploration/services/CommandQueueService.java
@@ -12,7 +12,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class CommandQueueService {
+
+/**
+ * An implementation of {@link ICommandQueueService} that
+ * handles and validates all commands staged in the queue.
+ */
+public class CommandQueueService implements ICommandQueueService {
 
     private static final Logger LOG = LoggerFactory.getLogger(CommandQueueService.class);
     private List<Command> commands;
@@ -24,10 +29,11 @@ public class CommandQueueService {
 
 
     /**
-     * Takes a of {@link List<Command>} commands which
+     * Takes a of {@link List} commands which
      * was passed into the simulator to populate the queue.
      *
      */
+    @Override
     public void initialize() {
         queue.addAll(commands);
     }
@@ -38,16 +44,19 @@ public class CommandQueueService {
      *
      * @return true or false.
      */
+    @Override
     public boolean placeCommandIsHeadEnqueued() {
         return queue.peek().name.equals("PLACE");
     }
 
     /**
      * Checks if the command at the peek of the queue is a at origin.
-     * A {@link CommandType#PLACE} is said to be at origin if it's coordinates are <tt>(0,0)<tt/>
+     * A {@link CommandType#PLACE} is said to be at origin if it's coordinates are <tt>(0,0)</tt>
      *
+     * @return the evaluation value as either true or false.
      * @throws IllegalArgumentException if the specified command is not at origin.
      */
+    @Override
     public boolean isPlaceCommandAtOrigin(){
         Command command = queue.peek();
         boolean result = command.getxAxis() == 0 && command.getyAxis() == 0;
@@ -63,8 +72,10 @@ public class CommandQueueService {
      * Checks the coordinates of the PLACE, BLOCK and EXPLORE commands passed into the system.
      * This check ensures the coordinates are valid and not out of grid bounds.
      *
+     * @return the evaluation value as either true or false.
      * @throws CoordinateOutsideBoundsException if the specified command is out of bounds.
      */
+    @Override
     public boolean validateCoordinates() {
         for (Command command : queue) {
             final String placeCmd = CommandType.PLACE.toString();
@@ -86,8 +97,10 @@ public class CommandQueueService {
      * Checks all the commands which has been passed to the simulator for execution.
      * This check uses regular expression and certain rules depending on the {@link CommandType}
      *
+     * @return the evaluation value as either true or false.
      * @throws InvalidSyntaxException which extends {@link RuntimeException} if the specified command is invalid.
      */
+    @Override
     public boolean validateQueueCommandSyntax() {
 
         return true;
@@ -98,6 +111,7 @@ public class CommandQueueService {
      *
      * @return reference to the queue
      */
+    @Override
     public Queue<Command> getQueue() {
         return this.queue;
     }
