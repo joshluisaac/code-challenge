e06f385be4909f10ab6c572594f1392a055e2988
joshua <nwankwo.joshua@gmail.com>
2019-03-17 09:25:39 +1100

    refactoring updates

--- src/main/java/com/codechallenge/pwc/au/AddressBookAppCLI.java
+++ src/main/java/com/codechallenge/pwc/au/AddressBookAppCLI.java
@@ -5,15 +5,21 @@ import com.codechallenge.pwc.au.entities.Contact;
 import com.codechallenge.pwc.au.persistence.AddressBookDao;
 import com.codechallenge.pwc.au.services.AddressBookService;
 import com.codechallenge.pwc.au.utils.JsonUtils;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class AddressBookAppCLI {
 
     private final AddressBookService addressBookService;
+
+    private static final String ADDRESS_BOOK_DATA_BASE = "book.json";
 
     public AddressBookAppCLI(AddressBookService addressBookService) {
         this.addressBookService = addressBookService;
@@ -40,22 +46,26 @@ public class AddressBookAppCLI {
 
     public static void main(String[] args) throws Exception {
         Contact contact = new Contact("Joshua","0479109809");
-        Contact contact2 = new Contact("Zoe","0479109800");
-        Contact contact3 = new Contact("Jeff","0479109801");
-        Contact contact4 = new Contact("Henry","0479109802");
+        //Contact contact2 = new Contact("Zoe","0479109800");
+        //Contact contact3 = new Contact("Jeff","0479109801");
+        //Contact contact4 = new Contact("JoshuA","0479109802");
+
+        /*Deserialize the JSON file into Java objects*/
+        SortedMap<String,String> database = new JsonUtils().fromJson(new FileReader(new File(ADDRESS_BOOK_DATA_BASE)), new TypeToken<SortedMap<String,String>>(){}.getType());
 
 
-        Map<String,String> existingContacts = new TreeMap<>(String::compareToIgnoreCase);
+        SortedMap<String,String> existingContacts = new TreeMap<>(String::compareToIgnoreCase);
+
+
+
+        for(SortedMap.Entry<String,String> entry : database.entrySet()) existingContacts.put(entry.getKey(), entry.getValue());
 
         AddressBookAppCLI cli = new AddressBookAppCLI(new AddressBookService(new AddressBookDao(),new AddressBook(existingContacts)));
         cli.execute(contact);
-        cli.execute(contact2);
-        cli.execute(contact3);
-        cli.execute(contact4);
 
         cli.displayAddressBook();
 
-        cli.writeToCache(new File("book.json"),new JsonUtils().toJson(cli.getAddressBook()));
+        cli.writeToCache(new File(ADDRESS_BOOK_DATA_BASE),new JsonUtils().toJson(cli.getAddressBook()));
 
 
     }
--- src/main/java/com/codechallenge/pwc/au/services/AddressBookService.java
+++ src/main/java/com/codechallenge/pwc/au/services/AddressBookService.java
@@ -4,6 +4,8 @@ import com.codechallenge.pwc.au.entities.AddressBook;
 import com.codechallenge.pwc.au.entities.Contact;
 import com.codechallenge.pwc.au.persistence.IDao;
 import com.codechallenge.pwc.au.utils.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +14,8 @@ import java.nio.file.Files;
 import java.util.Map;
 
 public class AddressBookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AddressBookService.class);
 
 
     private final IDao dao;
@@ -43,6 +47,7 @@ public class AddressBookService {
 
     public void displayAddressBook(){
         for(Map.Entry<String, String> entry : getAddressBook().entrySet()) {
+            LOG.info("{} {}", entry.getKey(), entry.getValue());
             System.out.println(entry.getKey() +" "+ entry.getValue());
         }
     }
