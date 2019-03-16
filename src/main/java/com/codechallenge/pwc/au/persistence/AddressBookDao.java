d91187927a1f48e76ca19df42e5a035232946820
joshua <nwankwo.joshua@gmail.com>
2019-03-16 21:14:00 +1100

    Refactored dao interface and concrete class

--- src/main/java/com/codechallenge/pwc/au/persistence/AddressBookDao.java
+++ src/main/java/com/codechallenge/pwc/au/persistence/AddressBookDao.java
@@ -1,14 +1,31 @@
 package com.codechallenge.pwc.au.persistence;
 
+import com.codechallenge.pwc.au.entities.AddressBook;
+import com.codechallenge.pwc.au.entities.Contact;
+
+import java.util.List;
+import java.util.Map;
+
 public class AddressBookDao implements IDao {
 
-    @Override
-    public void saveAddress() {
 
+    @Override
+    public void update(final Map<String,String> map, final Contact contact){
+        map.replace(contact.getName(),contact.getPhoneNumber());
     }
 
     @Override
-    public void updateAddress() {
+    public void add(final Map<String,String> map, final Contact contact) {
+        map.put(contact.getName(),contact.getPhoneNumber());
+    }
+
+
+    public List<Contact> queryAddressBook(){
+        return null;
+    }
+
+
+    public void mergeAddress(final AddressBook addressBook) {
 
     }
 }
--- src/main/java/com/codechallenge/pwc/au/persistence/IDao.java
+++ src/main/java/com/codechallenge/pwc/au/persistence/IDao.java
@@ -1,8 +1,15 @@
 package com.codechallenge.pwc.au.persistence;
 
+import com.codechallenge.pwc.au.entities.AddressBook;
+import com.codechallenge.pwc.au.entities.Contact;
+
+import java.util.Map;
+
 public interface IDao {
 
-    void saveAddress();
-    void updateAddress();
+    //void save(final Contact contact);
+    void update(final Map<String,String> map, final Contact contact);
+    void add(final Map<String,String> map, final Contact contact);
+    //void mergeAddress(final AddressBook addressBook);
 
 }
