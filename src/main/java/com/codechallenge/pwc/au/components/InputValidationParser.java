1c7dbfa37cf29c7420d61d330b2e1c7075cc3f69
Josh Uzo <nwankwo.joshua@gmail.com>
2019-03-17 20:11:23 +0800

    Implemented validator for JSON string

--- src/main/java/com/codechallenge/pwc/au/components/InputValidationParser.java
+++ src/main/java/com/codechallenge/pwc/au/components/InputValidationParser.java
@@ -1,4 +1,29 @@
 package com.codechallenge.pwc.au.components;
 
+import com.codechallenge.pwc.au.utils.JsonUtils;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.SortedMap;
+
 public class InputValidationParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InputValidationParser.class);
+
+    public static boolean isValidJson(final String maybeJson) {
+        boolean isValid = false;
+        try {
+            new JsonUtils().fromJson(maybeJson, new TypeToken<SortedMap<String, String>>() {
+            }.getType());
+            isValid = true;
+        } catch (JsonSyntaxException ex) {
+            LOG.error("{} is not a valid JSON", maybeJson);
+
+        }
+        return isValid;
+    }
+
+
 }
