30eb62fb3de3e2065445097caa86cd99d1483709
Josh Uzo <nwankwo.joshua@gmail.com>
2019-03-17 17:25:46 +0800

    Added address book implementation and interface

--- src/main/java/com/codechallenge/pwc/au/services/AddressBookUnionService.java
+++ src/main/java/com/codechallenge/pwc/au/services/AddressBookUnionService.java
@@ -1,10 +1,36 @@
 package com.codechallenge.pwc.au.services;
 
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * This class takes two address books and resolves the uniqueness between them
  */
-public class AddressBookUnionService {
+public class AddressBookUnionService implements IAddressBookUnionService {
 
+
+    /**
+     * Takes two {@link Map} and resolves the difference between them of strings and removes duplicate elements in that list
+     *
+     * @param addressBook1 address book 1
+     * @param addressBook2 address book 2
+     * @return unique list of items
+     */
+
+    @Override
+    public SortedMap<String, String> union(Map<String, String> addressBook1, Map<String, String> addressBook2) {
+        SortedMap<String, String> unionMap = new TreeMap<>(String::compareToIgnoreCase);
+
+        for (Map.Entry<String, String> entry : addressBook1.entrySet()) {
+            if (!addressBook2.containsKey(entry.getKey())) unionMap.put(entry.getKey(), entry.getValue());
+        }
+
+        for (Map.Entry<String, String> entry : addressBook2.entrySet()) {
+            if (!addressBook1.containsKey(entry.getKey())) unionMap.put(entry.getKey(), entry.getValue());
+        }
+        return unionMap;
+    }
 
 
 }
--- src/main/java/com/codechallenge/pwc/au/services/IAddressBookUnionService.java
+++ src/main/java/com/codechallenge/pwc/au/services/IAddressBookUnionService.java
@@ -1,4 +1,9 @@
 package com.codechallenge.pwc.au.services;
 
+import java.util.Map;
+import java.util.SortedMap;
+
 public interface IAddressBookUnionService {
+
+    public SortedMap<String,String> union(Map<String, String> addressBook1, Map<String, String> addressBook2);
 }
